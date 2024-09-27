35f9f7ab6c1fa6eb81a6cddfc7e2487d36fa15ea
InfinyTech <contact@infinytech.io>
2024-09-27 12:35:00 +0200

    Commit apres mentor

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,3 +1,39 @@
+fetch("http://localhost:5678/api/categories/")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json(); // ou response.text() si c'est du texte
+  })
+  .then((categories) => {
+    console.log(categories); // Affiche les données récupérées
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
+fetch("http://localhost:5678/api/works/")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json(); // ou response.text() si le retour est en texte
+  })
+  .then((data) => {
+    const works = data.works; // Assumons que les "works" se trouvent dans cette propriété
+    if (Array.isArray(works)) {
+      works.forEach((work) => {
+        // Logique pour traiter chaque "work"
+        console.log("Work:", work);
+      });
+    } else {
+      console.log("No works found");
+    }
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
 // Js pour les filtres
 function filterItems(category) {
   var items = document.getElementsByClassName("item");
